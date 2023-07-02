c55a113d4b93ee05bc2b43adad39415bad4b8ec3
Omari Hopkins <91702475+OmariHop@users.noreply.github.com>
2023-07-01 23:08:37 -0400

    Create solution.py
    HashMap solution

---
+++ MajorityElement/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        count = {}
+        res, maxCount = 0,0
+        
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+            res = n if count[n] > maxCount else res
+            maxCount = max(count[n], maxCount)
+
+        return res
+
+
+
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
